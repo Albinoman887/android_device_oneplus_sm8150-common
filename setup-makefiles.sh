44b0c2133773ff1074d346f020eeda5c6843e1af
Matt Filetto <matt.filetto@gmail.com>
2020-08-30 19:00:43 -0700

    Revert "hotdogg: Update extract files for hotdogg"
    This reverts commit ddf78b42d55ab12567d218d9d3fa075096139694.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,11 +17,42 @@
 
 set -e
 
-# Required!
-export DEVICE=hotdogg
-export DEVICE_COMMON=sm8150-common
-export VENDOR=oneplus
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-export DEVICE_BRINGUP_YEAR=2019
+LINEAGE_ROOT="${MY_DIR}/../../.."
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper for common
+setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
+
+# Copyright headers and guards
+write_headers "guacamole guacamoleb guacamoleg hotdog hotdogg hotdogb"
+
+# The standard common blobs
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
+
+if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+    # Reinitialize the helper for device
+    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false
+
+    # Copyright headers and guards
+    write_headers
+
+    # The standard device blobs
+    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
+
+    # Finish
+    write_footers
+fi
